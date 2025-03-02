270da5ec8e5c7c94e465a56dc08781f86617f703
Elaine Coughlan <20067964@mail.wit.ie>
2025-03-02 21:24:21 +0000

    Added Splash screen

--- app/src/main/java/com/elaine/minerecipies/MainActivity.kt
+++ app/src/main/java/com/elaine/minerecipies/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.elaine.minerecipies.navigation.NavHostProvider
 import com.elaine.minerecipies.ui.components.AppDrawer
@@ -21,6 +22,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             MineRecipiesTheme {
                 Surface(
