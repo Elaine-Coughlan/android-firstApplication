8bd5e233433882f8484e628fc7e619b8e937f9d9
Elaine Coughlan <20067964@mail.wit.ie>
2025-03-08 15:13:42 +0000

    Warnings removed or checked

--- app/src/main/java/com/elaine/minerecipies/ui/screens/RecipeScreen.kt
+++ app/src/main/java/com/elaine/minerecipies/ui/screens/RecipeScreen.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.elaine.minerecipies.ui.components.RecipeItem
 import com.elaine.minerecipies.ui.theme.MineRecipiesTheme
